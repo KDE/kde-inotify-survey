8782cf08a27cf9cb4aeb923070fdb3bfcb6bf9b1
Harald Sitter <sitter@kde.org>
2022-05-10 11:20:26 +0200

    add includes for getuid

--- src/kded/kded.cpp
+++ src/kded/kded.cpp
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 // SPDX-FileCopyrightText: 2020-2022 Harald Sitter <sitter@kde.org>
 
+#include <sys/types.h>
+#include <unistd.h>
+
 #include <chrono>
 
 #include <QTimer>
